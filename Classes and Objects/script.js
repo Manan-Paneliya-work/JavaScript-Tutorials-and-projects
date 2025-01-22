64f2684445e6568504a9817fe92a5e4d52545795
Manan-Paneliya-Work <mananpaneliya0@gmail.com>
2025-01-22 08:15:19 +0530

    commited

---
+++ Classes and Objects/script.js
@@ -0,0 +1,64 @@
+
+
+//1) function decelaration
+// function greet() { 
+//     console.log("Hello, World 1");
+// }
+
+
+//2) Function Expression
+// const greet = function() {
+//     console.log("Hello, World 12");
+// };
+
+
+//3) Arrow Function 
+// const greet = () => {
+//     console.log("Hello, World 123");
+// };
+
+
+// 4) Immediately Invoked function Expression(IIFE).
+// (function() {
+//     console.log("Hello, World 1234");
+// })();
+
+
+//5) Constructor Function
+// function Person(name) {
+//     this.name = name;
+// }
+// const person1 = new Person("Alice");
+// console.log(person1);
+
+
+
+//6) Methods in Objects
+// const obj = {
+//     greet: function() {
+//         console.log("Hello, World 12345");
+//     }
+// };
+
+//OR 
+
+// const obj = {
+//     greet(){
+//         console.log("Hello, World 123456");
+//     }
+// }
+
+
+//7) Class Methods
+// class Person{
+//     greet(){
+//         console.log("Hello, World 1234567");
+//     }
+// }
+// const person = new Person();
+// person.greet();
+
+
+// console.log(obj.greet());
+
+// greet();
